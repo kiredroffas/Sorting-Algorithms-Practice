4960d0dc724df2dee4c44a806250e66da296c6a2
Erik <erik.safford@wsu.edu>
2019-10-17 15:49:58 -0700

    Implemented Selection Sort

---
+++ selectionSort.js
@@ -0,0 +1,75 @@
+/*
+Selection Sort: O(n^2)
+(worse version of insertion sort)
+
+The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and 
+putting it at the beginning. The algorithm maintains two subarrays in a given array.
+1) The subarray which is already sorted.
+2) Remaining subarray which is unsorted.
+
+In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray is picked and moved to 
+the sorted subarray.
+
+Following example explains the above steps:
+
+arr[] = 64 25 12 22 11
+
+// Find the minimum element in arr[0...4]
+// and place it at beginning
+11 25 12 22 64
+
+// Find the minimum element in arr[1...4]
+// and place it at beginning of arr[1...4]
+11 12 25 22 64
+
+// Find the minimum element in arr[2...4]
+// and place it at beginning of arr[2...4]
+11 12 22 25 64
+
+// Find the minimum element in arr[3...4]
+// and place it at beginning of arr[3...4]
+11 12 22 25 64 
+*/
+
+function swap(arr, a, b) {
+    let temp = arr[a]
+    arr[a] = arr[b]
+    arr[b] = temp
+}
+
+function selectionSort(arr) {
+    let subarray = 0  //index of sorted subarray
+    let stop = arr.length  //Index to stop sorting
+    
+    //Cycle through each position of the array, increasing subarray with each pass
+    while (subarray != stop) {  
+        lowest = subarray  //Current index of lowest value in unsorted subarray
+        console.log("arr is: " + arr + " - subarray at position: " + subarray)
+
+        //Find lowest value in unsorted subarray to swap with
+        for (let i = subarray; i < arr.length; i++) {
+            if (arr[i] < arr[subarray] && arr[i] < arr[lowest]) {
+                console.log(arr[i] + " is less then " + arr[subarray])
+                lowest = i
+            }
+        }
+
+        //Swap lowest value with value at index subarray, if neccessary
+        if (arr[lowest] != arr[subarray]) {
+            console.log(arr[lowest] + " is lowest unsorted value, swapping " + arr[lowest] + ' with ' + arr[subarray] + "\n")
+            swap(arr, lowest, subarray)
+        }
+        else {
+            console.log("arr[subarray] = arr[lowest] (" + arr[lowest] + ") , no swap needed\n")
+        }
+
+        //Incease sorted subarray
+        subarray++
+    }
+    return arr
+}
+
+let arr = [64, 25, 12, 22, 11]
+console.log("arr before: " + arr + "\n")
+let sortedarr = selectionSort(arr)
+console.log("arr after: " + sortedarr)
