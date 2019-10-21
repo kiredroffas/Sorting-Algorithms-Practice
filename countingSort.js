7088937f3d5f1ec08f2b9d9e3cdfff4013805ed5
Erik <erik.safford@wsu.edu>
2019-10-21 13:44:32 -0700

    added time complexity

--- countingSort.js
+++ countingSort.js
@@ -1,4 +1,6 @@
 /*
+Counting Sort: O(n + k), n total array items, k range of items in array
+
 Counting sort is a sorting technique based on keys between a specific range. 
 It works by counting the number of objects having distinct key values (kind of hashing). 
 Then doing some arithmetic to calculate the position of each object in the output sequence.
